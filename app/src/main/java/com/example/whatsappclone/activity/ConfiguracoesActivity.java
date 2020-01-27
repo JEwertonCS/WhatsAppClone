4b797f4efc5740d12551dd4433448b500abab69f
Ewerton Costa <jecs.ewerton@gmail.com>
2020-01-27 14:30:41 -0300

    Criação das activity de Configurações

---
+++ app/src/main/java/com/example/whatsappclone/activity/ConfiguracoesActivity.java
@@ -0,0 +1,23 @@
+package com.example.whatsappclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.whatsappclone.R;
+
+public class ConfiguracoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle( "Configurações" );
+        setSupportActionBar( toolbar );
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+    }
+}
--- app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.whatsappclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,7 +12,12 @@ import android.view.MenuItem;
 
 import com.example.whatsappclone.R;
 import com.example.whatsappclone.config.ConfiguracaoFirebase;
+import com.example.whatsappclone.fragment.ContatoFragment;
+import com.example.whatsappclone.fragment.ConversaFragment;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle( "WhatApp Clone" );
         setSupportActionBar( toolbar );
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.fragment_conversa, ConversaFragment.class)
+                .add(R.string.fragment_contato, ContatoFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout smartTabLayout = (SmartTabLayout) findViewById(R.id.smartTabLayout);
+        smartTabLayout.setViewPager(viewPager);
+
     }
 
     @Override
@@ -42,9 +62,18 @@ public class MainActivity extends AppCompatActivity {
                 deslogarUsuario();
                 finish();
                 break;
+
+            case R.id.configuracao:
+                abrirConfiguracoes();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void abrirConfiguracoes() {
+        Intent intent = new Intent( MainActivity.this, ConfiguracoesActivity.class);
+        startActivity( intent );
     }
 
     private void deslogarUsuario() {
